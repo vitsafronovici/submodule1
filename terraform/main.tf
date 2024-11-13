16d810fe9395a55a51e701a00525d225d5b2179c
Vitalie Safronovici <vitalie.safronovici@harness.io>
2024-11-13 13:19:09 +0200

    added vars

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,10 @@ provider "aws" {
   region                   = "us-east-1"
 }
 
+variable "myvar" {
+  default = "us-east-1"
+}
+
 resource "aws_instance" "ec2_example" {
     ami = "ami-0305d0b03812a425e"
     instance_type = "t2.micro"
@@ -10,12 +14,7 @@ resource "aws_instance" "ec2_example" {
     }
 }
 
-terraform {
-    backend "s3" {
-        bucket = "vit-s3-bucket"
-        key    = "vit/terraform/remote/s3/terraform.tfstate"
-        region     = "us-east-1"
-        dynamodb_table = "vit-lock"
-    }
+output "myvar" {
+  value = "${var.myvar}"
 }
 
